a8db64d4daeeada6cfa32960e3499f0879c3e9d6
tullyz <50986699+tullyz@users.noreply.github.com>
2021-01-23 14:57:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "installing mouse for Volumio"
 npm install node-mouse
-npm install socket.io-client
+npm install socket.io-client@2.3.1
 npm install input-event
 npm install -g forever
 /lib/node_modules/forever/bin/forever start /home/volumio/mouse-volume/index.js
