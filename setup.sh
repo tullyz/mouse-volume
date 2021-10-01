ab7ea9c202cd132bbdf2917e1bd030bce5c7921d
tullyz <50986699+tullyz@users.noreply.github.com>
2021-10-01 11:11:41 +0900

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 echo "installing mouse for Volumio"
 npm install node-mouse
-npm install socket.io-client@2.3.1
+npm install socket.io-client
 npm install input-event
-npm install -g forever
-/lib/node_modules/forever/bin/forever start /home/volumio/mouse-volume/index.js
+node /home/volumio/mouse-volume/index.js
 cp rc.local /etc
 chmod 755 /etc/rc.local
 
