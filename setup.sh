bdfb06f63990bc15d85a004d3b95184f2a4432ee
Shinji F. Tanaka <50986699+tullyz@users.noreply.github.com>
2022-05-21 20:43:45 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo "installing mouse for Volumio"
 npm install node-mouse
 npm install socket.io-client@2.3.1
 npm install input-event
+npm install --save-dev webpack-remove-debug
 node /home/volumio/mouse-volume/index.js &
 cp rc.local /etc
 chmod 755 /etc/rc.local
