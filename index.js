b08ae1ec6f040fcdfb3bda00bb02901d8683bfa5
Shinji F. Tanaka <50986699+tullyz@users.noreply.github.com>
2025-03-16 11:58:30 +0900

    Revert "Update index.js"
    This reverts commit 823a9463be06fda5eabe54f9f91112a992be1e18.

--- index.js
+++ index.js
@@ -1,87 +1,84 @@
+var inputevent = require('input_event');
+//var inputevent = require('input-event');
 
-var Mouse = require('node-mouse');
-var mouseDevice = new Mouse();
+// find # of PowerMate event
+const execSync = require('child_process').execSync;
+const result = execSync('cat /proc/bus/input/devices | grep Handlers=event').toString();
+n = result.indexOf("event");
+var a = result.substring(n + 5, n + 6);
+var pm = new inputevent('event' + a);
+//var pm = new inputevent('event1');
+// 'event1' is the file corresponding to my PowerMate in /dev/input/
+// cat /proc/bus/input/devices
 
-var InputEvent = require('input-event');
-
-var io=require('socket.io-client');
-var socket= io.connect('http://localhost:3000');
+var io = require('socket.io-client');
+var socket = io.connect('http://localhost:3000');
 
 // set initial volume value
-var vol = 15 ;  
+var vol = 3;
 socket.emit('volume', vol);
 
-// find # of mouse event
-const execSync = require('child_process').execSync;
-const result =  execSync('cat /proc/bus/input/devices | grep mouse0').toString();
-n = result.indexOf("event")
-var a = result.substring(n+5, n+6)
-var input = new InputEvent('/dev/input/event'+a);
+// Function to check Volumio state and play
+function checkVolumioState() {
+  socket.emit('getState', '');
+  socket.on('pushState', function (data) {
+    if (data.status === 'play' || data.status === 'stop') {
+      console.log('MPD is ready, sending play command...');
+      socket.emit('play');
+    } else {
+      console.log('MPD not ready, retrying...');
+      setTimeout(checkVolumioState, 5000); // Retry every 5 seconds
+    }
+  });
+}
 
-var eventDevice = new InputEvent.Mouse(input);
-
+// Start checking Volumio state after ensuring WebSocket connection
+socket.on('connect', function () {
+  console.log('WebSocket connected! Checking Volumio state...');
+  checkVolumioState();
+});
 
 // Create play lists if empty
 socket.emit('listPlaylist');
-socket.on('pushListPlaylist', function(data) {
-   if (data.length == 0) {
-
-    socket.emit('createPlaylist',{"name":"Venice Classic Radio"});
-    socket.emit('addToPlaylist',{
-	"name":"Venice Classic Radio", 
-	"service":"webradio", 
-        "uri":"http://174.36.206.197:8000"
-//        "uri":"https://uk2.streamingpulse.com/ssl/vcr1"
-//        "uri":"https://uk2.streamingpulse.com/ssl/vcr2"
-	});
-    socket.emit('createPlaylist',{"name":"Capital FM"});
-    socket.emit('addToPlaylist',{
-	"name":"Capital FM", 
-	"service":"webradio", 
-	//"uri":"http://media-ice.musicradio.com/CapitalMP3"
-	"uri":"https://media-ssl.musicradio.com/Capital"
-	});
-
-    }
+socket.on('pushListPlaylist', function (data) {
+  if (data.length == 0) {
+    socket.emit('createPlaylist', { "name": "Venice Classic Radio" });
+    socket.emit('addToPlaylist', {
+      "name": "Venice Classic Radio",
+      "service": "webradio",
+      "uri": "https://uk2.streamingpulse.com/ssl/vcr1"
+    });
+    socket.emit('createPlaylist', { "name": "Capital FM" });
+    socket.emit('addToPlaylist', {
+      "name": "Capital FM",
+      "service": "webradio",
+      "uri": "https://media-ssl.musicradio.com/Capital"
+    });
+  }
 });
 
-var playstate = 1 ;
+var playstate = 2;
 
-mouseDevice.on('click', function(e){
-  if (e.button === 0) {
-//    console.log("left click");
-    socket.emit('playPlaylist',{name:"Venice Classic Radio"});
-  }
-  if (e.button === 2) {
-//    console.log("right click");
-    socket.emit('playPlaylist',{name:"Capital FM"});
-  }
-  if (e.button === 1) {
-//    console.log("wheel click");
-//    socket.emit('prev');
-//    socket.emit('next');
+pm.on('keypress', pm => {
   if (playstate == 1) {
-	socket.emit('pause');
-	playstate = 0
-	}
-  else {
-	socket.emit('play');
-	playstate = 1 
-	}
+    socket.emit('playPlaylist', { name: "Venice Classic Radio" });
+    playstate = 2;
+  } else {
+    socket.emit('playPlaylist', { name: "Capital FM" });
+    playstate = 1;
   }
 });
 
-// volume setting    
-eventDevice.on('wheel', e => {
-  var direction = e.value === 1 ? 1 : -1
-// get current volume value
- socket.emit('getState', '');
- socket.on('pushState', function(data) {
-   vol = data.volume ;  
- });
-  vol = vol + direction ;
-// volume limitter
-  if (vol < 0) vol = 0 ;
-  if (vol > 50) vol = 50 ;
+pm.on('rel', pm => {
+  var direction = pm.value === 1 ? 1 : -1;
+  // get current volume value
+  socket.emit('getState', '');
+  socket.on('pushState', function (data) {
+    vol = data.volume;
+  });
+  vol = vol + direction;
+  // volume limiter
+  if (vol < 0) vol = 0;
+  if (vol > 100) vol = 100;
   socket.emit('volume', vol);
-})
+});
