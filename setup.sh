e31a459e483fa95ed317c924001c56e441ba6a55
Shinji F. Tanaka <50986699+tullyz@users.noreply.github.com>
2025-03-16 14:55:40 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ echo "installing mouse for Volumio"
 npm install node-mouse
 npm install socket.io-client@2.3.1
 npm install input-event
-npm install --save-dev webpack-remove-debug
 node /home/volumio/mouse-volume/index.js &
-cp rc.local /etc
-chmod 755 /etc/rc.local
+#cp rc.local /etc
+APPEND_TEXT="sleep 40s\nnode /home/volumio/mouse-volume/index.js &"
+sudo sed -i -e "/exit 0/i $APPEND_TEXT" "/etc/rc.local"
+#chmod 755 /etc/rc.local
 touch /data/manifestUI
