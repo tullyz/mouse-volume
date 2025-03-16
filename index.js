ae97066663f2fce77808e687499803f1b9850c3c
Shinji F. Tanaka <50986699+tullyz@users.noreply.github.com>
2025-03-16 11:58:47 +0900

    Revert "Update index.js"
    This reverts commit 0d57fd6ff94060155c828b43f1aaadcc47ec42d1.

--- index.js
+++ index.js
@@ -1,84 +1,85 @@
-var inputevent = require('input_event_s');
-//var inputevent = require('input-event');
+﻿
+var Mouse = require('node-mouse');
+var mouseDevice = new Mouse();
 
-// find # of PowerMate event
+var InputEvent = require('input-event');
+// find # of mouse event
 const execSync = require('child_process').execSync;
-const result = execSync('cat /proc/bus/input/devices | grep Handlers=event').toString();
-n = result.indexOf("event");
-var a = result.substring(n + 5, n + 6);
-var pm = new inputevent('event' + a);
-//var pm = new inputevent('event1');
-// 'event1' is the file corresponding to my PowerMate in /dev/input/
-// cat /proc/bus/input/devices
+const result =  execSync('cat /proc/bus/input/devices | grep mouse0').toString();
+n = result.indexOf("event")
+var a = result.substring(n+5, n+6)
+var input = new InputEvent('/dev/input/event'+a);
 
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:3000');
+var eventDevice = new InputEvent.Mouse(input);
+
+var io=require('socket.io-client');
+var socket= io.connect('http://localhost:3000');
 
 // set initial volume value
-var vol = 3;
+var vol = 3 ;  
 socket.emit('volume', vol);
 
-// Function to check Volumio state and play
-function checkVolumioState() {
-  socket.emit('getState', '');
-  socket.on('pushState', function (data) {
-    if (data.status === 'play' || data.status === 'stop') {
-      console.log('MPD is ready, sending play command...');
-      socket.emit('play');
-    } else {
-      console.log('MPD not ready, retrying...');
-      setTimeout(checkVolumioState, 5000); // Retry every 5 seconds
-    }
-  });
-}
-
-// Start checking Volumio state after ensuring WebSocket connection
-socket.on('connect', function () {
-  console.log('WebSocket connected! Checking Volumio state...');
-  checkVolumioState();
-});
+// auto start
+socket.emit('play');
 
 // Create play lists if empty
 socket.emit('listPlaylist');
-socket.on('pushListPlaylist', function (data) {
-  if (data.length == 0) {
-    socket.emit('createPlaylist', { "name": "Venice Classic Radio" });
-    socket.emit('addToPlaylist', {
-      "name": "Venice Classic Radio",
-      "service": "webradio",
-      "uri": "https://uk2.streamingpulse.com/ssl/vcr1"
-    });
-    socket.emit('createPlaylist', { "name": "Capital FM" });
-    socket.emit('addToPlaylist', {
-      "name": "Capital FM",
-      "service": "webradio",
-      "uri": "https://media-ssl.musicradio.com/Capital"
-    });
-  }
+socket.on('pushListPlaylist', function(data) {
+   if (data.length == 0) {
+
+    socket.emit('createPlaylist',{"name":"Venice Classic Radio"});
+    socket.emit('addToPlaylist',{
+	"name":"Venice Classic Radio", 
+	"service":"webradio", 
+	"uri":"https://uk2.streamingpulse.com/ssl/vcr1"
+	});
+    socket.emit('createPlaylist',{"name":"Capital FM"});
+    socket.emit('addToPlaylist',{
+	"name":"Capital FM", 
+	"service":"webradio", 
+	"uri":"https://media-ssl.musicradio.com/Capital"
+	});
+
+    }
 });
 
-var playstate = 2;
+var playstate = 1 ;
 
-pm.on('keypress', pm => {
+mouseDevice.on('click', function(e){
+  if (e.button === 0) {
+//    console.log("left click");
+    socket.emit('playPlaylist',{name:"Venice Classic Radio"});
+  }
+  if (e.button === 2) {
+//    console.log("right click");
+    socket.emit('playPlaylist',{name:"Capital FM"});
+  }
+  if (e.button === 1) {
+//    console.log("wheel click");
+//    socket.emit('prev');
+//    socket.emit('next');
   if (playstate == 1) {
-    socket.emit('playPlaylist', { name: "Venice Classic Radio" });
-    playstate = 2;
-  } else {
-    socket.emit('playPlaylist', { name: "Capital FM" });
-    playstate = 1;
+	socket.emit('pause');
+	playstate = 0
+	}
+  else {
+	socket.emit('play');
+	playstate = 1 
+	}
   }
 });
 
-pm.on('rel', pm => {
-  var direction = pm.value === 1 ? 1 : -1;
-  // get current volume value
-  socket.emit('getState', '');
-  socket.on('pushState', function (data) {
-    vol = data.volume;
-  });
-  vol = vol + direction;
-  // volume limiter
-  if (vol < 0) vol = 0;
-  if (vol > 100) vol = 100;
+// volume setting    
+eventDevice.on('wheel', e => {
+  var direction = e.value === 1 ? 1 : -1
+// get current volume value
+ socket.emit('getState', '');
+ socket.on('pushState', function(data) {
+   vol = data.volume ;  
+ });
+  vol = vol + direction ;
+// volume limitter
+  if (vol < 0) vol = 0 ;
+  if (vol > 50) vol = 50 ;
   socket.emit('volume', vol);
-});
+})
